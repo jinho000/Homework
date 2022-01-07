d7c722fd9b09fc5cc2421ccdd87068fce9b7e244
jinho <jinho74350721@gmail.com>
2022-01-07 16:26:02 +0900

    ip 체크 함수

--- Homework220107/TestProject/TestProject/TestProject.cpp
+++ Homework220107/TestProject/TestProject/TestProject.cpp
@@ -1,20 +1,69 @@
-﻿// TestProject.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+bool CheckIP(std::string& _ip, int& port)
+{
+	std::string ip = _ip;
+	int idx = 0;
+	int addr[3] = {0, };
+	for (int i = 0; i < 3; ++i)
+	{
+		int findIdx = ip.find('.', idx);
+		if (findIdx == std::string::npos)
+		{
+			return false;
+		}
+
+		addr[i] = std::stoi(ip.substr(ip.length() - findIdx));
+		++findIdx;
+		ip = ip.substr(findIdx);
+
+		if (0 > addr[i] || 255 < addr[i])
+		{
+			return false;
+		}
+	}
+
+	// 포트 확인
+	int portIdx = ip.find(':', idx);
+	if (std::string::npos == portIdx)
+	{
+		std::cout << "포트를 입력해주세요" << std::endl;
+		std::cin >> port;
+		_ip += ":";
+		_ip += std::to_string(port);
+	}
+	else
+	{
+		++portIdx;
+		port = stoi(ip.substr(portIdx));
+	}
+
+	return true;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// 123.123.511.555:2333
+	// 
+	std::string ip;
+	int port = -1;
+	//ip = "123.123.213.123:3000";
+	while (true)
+	{
+		std::cout << "IP를 입력해주세요" << std::endl;
+		std::cin >> ip;
+		if (false == CheckIP(ip, port))
+		{
+			std::cout << "IP 인식 실패" << std::endl;
+		}
+		else
+		{
+			std::cout << "Ip:" << ip << std::endl;
+			std::cout << "Port: " << port << std::endl;
+			break;
+		}
+	}
+	
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
